392f07fa612c404d5224bfb7c036ae1c497a5abf
MatheusGorga <matheusgobar@gmail.com>
2023-06-15 20:28:23 -0300

    create list

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Formulario from './components/Form';
+import Lista from './components/List';
 
 function App() {
   return (
       <>
         <Formulario/>
+        <Lista/>
       </>
 
   );
