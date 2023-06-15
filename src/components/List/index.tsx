c060bed6d6930877daf4afd9730e436db3c5db0a
MatheusGorga <matheusgobar@gmail.com>
2023-06-15 20:36:26 -0300

    iteration with map in list

--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -1,27 +1,30 @@
 function Lista() {
-    return (
-      <aside>
-        <h2> Estudos do dia </h2>
-        <ul>
-          <li>
-            <h3>
-              React
-            </h3>
-            <span>
-              02:00:00
-            </span>
-          </li>
-          <li>
-            <h3>
-              Javascript
-            </h3>
-            <span>
-              01:00:00
-            </span>
-          </li>
-        </ul>
-      </aside>
-    )
-  }
+  const tarefas = [
+    {
+      tarefa: "React",
+      tempo: "02:00:00",
+    },
+    {
+      tarefa: "Javascript",
+      tempo: "01:00:00",
+    },
+  ];
 
-  export default Lista;
+  return (
+    <aside>
+      <h2> Estudos do dia </h2>
+      <ul>
+        {tarefas.map((item, indice) => {
+          return (
+            <li>
+              <h3>{item.tarefa}</h3>
+              <span>{item.tempo}</span>
+            </li>
+          );
+        })}
+      </ul>
+    </aside>
+  );
+}
+
+export default Lista;
